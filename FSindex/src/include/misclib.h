1de2b45c1d6e696feb71cc3d1ea5a0cc3f4c4fe2
Aleksandar Stojmirovic <aleksandar@stojmirovic.org>
2005-04-15 08:22:35 +0000

    Cleaned up long unused code.

--- FSindex/src/include/misclib.h
+++ FSindex/src/include/misclib.h
@@ -27,10 +27,8 @@ typedef signed long int SLINT;
 typedef unsigned short int USINT;
 typedef signed short int SSINT;
 
-typedef unsigned int UINT_t;
-typedef signed int SINT_t;
 
-/* Exception handling */
+/* Exception handling - using cexept */
 
 #define MSG_SIZE 512
 typedef struct 
@@ -60,62 +58,35 @@ extern struct exception_context the_exception_context[1];
 #define RUNTIME_ERR 11
 
 
+/* Printing into buffers */
 
 void absprintf(char **buf, int *size, int *len, const char *fmt, ...);
 
 
-
-
-
-/* Some numerical typedefs and functions from gamerf package and
-   otherwise */
-
-#ifndef DCOMPLEX
-struct dcomplex_ {
-    double re;
-    double im;
-};
-#define DCOMPLEX struct dcomplex_
-#define DREAL(x) (x).re
-#define DIMAG(x) (x).im
-#define DCMPLX(x,y,z) (z).re = x, (z).im = y
-#endif
-
-DCOMPLEX cdgamma(DCOMPLEX x);
-double dcbrt(double x);
-double derf(double x);
-double derfc(double x);
-double dgamma(double x);
-double dierfc(double y); 
-double dlgamma(double x);
-
-double dibeta(double p, double a, double b);
-
-/* Statistical functions - my own */
-void discrete_meanvar(int n, SSINT *values, double *probs, 
-		      double *mean, double *var); 
-
 /* Memory managment functions - error checking */
 
-char *newmem(long int number, long int size);
 void *callocec(long int number, long int size);
 void *mallocec(long int size);
 void *reallocec(void *pt, long int size);
-
-/* File functions - error checking */
-
-
 
 
 /* Auxillaries */
 int max(int a, int b);
 int min(int a, int b);
 
+/* String I/O */
+
 void fwrite_string(char *s, FILE *fp);
 void fread_string(char **s, FILE *fp);
 
+
+/* Progress bar */
+
 void printbar(FILE *outstream, ULINT cntr, ULINT dispc, 
 	      USINT dpr);
+
+
+/* Comparison functions */
 
 int compare_int(const void *M1, const void *M2);
 int compare_char(const void *M1, const void *M2);
@@ -130,50 +101,11 @@ int split_base_dir(const char *full_name, char **basename,
 int cat_base_dir(char **full_name, const char *basename, 
 		 const char *dirname);
  
-/* XML tag prinitng */
-void xml_open_tag(FILE *stream, const char *tag, int indent,
-		  int eol);
-
-
-typedef struct
-  {
-    SLINT no_bins;
-    SLINT min_bin;
-    ULINT *freq;
-    ULINT attainable_pts;
-    ULINT unattainable_pts;
-} SEED_HIST;
-
-SEED_HIST *seedhist_init(SLINT min_bin, SLINT max_bin);
-void seedhist_init1(SEED_HIST *hist, SLINT min_bin, SLINT max_bin);
-int seedhist_clear(SEED_HIST *hist);
-int seedhist_clear1(SEED_HIST *hist);
-void seedhist_add_acount(SEED_HIST *hist, SLINT bin);
-void seedhist_add_ucount(SEED_HIST *hist);
-int seedhist_merge(SEED_HIST *hist1, SEED_HIST *hist2);
-int seedhist_read(SEED_HIST *hist, FILE *stream);
-int seedhist_write(SEED_HIST *hist, FILE *stream);
-int seedhist_print(SEED_HIST *hist, FILE *stream);
-
-        
-/*#define seedhist_add_acount(hist, bin) \
-        ((hist)->freq[(bin)]++), \
-        ((hist)->attainable_pts++)*/
-
-#define seedhist_add_ucount(hist) \
-        ((hist)->unattainable_pts++)
-
-
-/* Getline implementation  from mailutils */
-extern int getline(char **_lineptr, size_t *_n, FILE *_stream);
-
-extern int getdelim(char **_lineptr, size_t *_n, int _delimiter, FILE *_stream);
-
 
 /********************************************************************/    
 /********************************************************************/    
 /***                                                              ***/
-/***               INLINE FUNCTION DEFINITIONS                    ***/ 
+/***               INLINE FUNCTION DEFINITIONS (Macros)           ***/ 
 /***                                                              ***/
 /********************************************************************/    
 /********************************************************************/    
@@ -196,95 +128,4 @@ extern int getdelim(char **_lineptr, size_t *_n, int _delimiter, FILE *_stream);
     }                                              \
   } while(0)        
 
-#define xml_open_tag(fp, tag, ind, eol) do {       \
-  fprintf((fp),"%*.*s<%s>",(ind),(ind),"",(tag));  \
-  if ((eol)) fprintf((fp), "\n");                  \
-  } while(0)                                       \
-
-#define xml_close_tag(fp, tag, ind) do {           \
-  fprintf((fp),"%*.*s</%s>\n",(ind),(ind),"",(tag));\
-  } while(0)                                       \
-
-
-
-#if 0
-#define e_malloc(ptr, size) do {                  \
-  if ((size) < 0)                                 \
-    return NEG_MEM_REQ;                           \
-  if (((ptr) = malloc(size)) == NULL)             \
-    return NO_MEM;                                \
-  } while(0)
-
-#define e_calloc(ptr, num, size) do {             \
-  if ((size)*(num) < 0)                           \
-    return NEG_MEM_REQ;                           \
-  if ((ptr = calloc((num), (size))) == NULL)      \
-    return NO_MEM;                                \
-  } while(0)
-
-#define e_realloc(ptr, size) do {                 \
-  if ((size) < 0)                                 \
-    return NEG_MEM_REQ;                           \
-  if (((ptr) = realloc((ptr), (size))) == NULL)   \
-    return NO_MEM;                                \
-  } while(0)
-
-#define e_free(ptr) do {                          \
-  free((ptr));                                    \
-  ptr = NULL;                                     \
-  } while(0)
-
-#define e_fopen(stream, path, mode) do {          \
-  if (((stream) = fopen((path), (mode))) == NULL) \
-    return FOPEN_ERR;                             \
-  } while(0)
-
-#define e_fclose(stream) do {                     \
-  if (fclose((stream)) == EOF)                    \
-    return FCLOSE_ERR;                            \
-  } while(0)
-
-#define check_err(status) do {                    \
-  if (status)                                     \
-    return status;                                \
-  } while(0)
-#endif
-
-
-#ifdef  MY_INLINE
-
-#if 0
-MY_INLINE
-int max(int a, int b)
-{
-  return a > b ? a : b;
-}
-
-MY_INLINE
-int min(int a, int b)
-{
-  return a < b ? a : b;
-}
-
-MY_INLINE
-void printbar(FILE *outstream, ULINT cntr, ULINT dispc, 
-	      USINT dpr)  
-{
-  char dot = '.';
-
-  if((cntr % dispc) == 0)
-    {
-      /* print '*' as every 10th dot */ 
-      if ((cntr % (10*dispc)) == 0) dot = '*'; 
-      fputc(dot, outstream);
-      if ((cntr % (dpr*dispc)) == 0) 
-        fprintf(outstream, " %ld\n", cntr);
-      fflush(outstream);
-    }
-}         
-#endif
-
-#endif /* MISCLIB_INLINE */   
-
-
-#endif
+#endif /* _MISCLIB_H */
--- FSindex/src/lib/misclib.c
+++ FSindex/src/lib/misclib.c
@@ -5,6 +5,12 @@
 #include <math.h>
 #include "misclib.h"
 
+/********************************************************************/ 
+/*                                                                  */
+/*                     Exception handling                           */ 
+/*                                                                  */
+/********************************************************************/ 
+
 EXCEPTION *FSexcept(int code, const char *fmt, ...)
 {
   va_list ap;
@@ -15,24 +21,13 @@ EXCEPTION *FSexcept(int code, const char *fmt, ...)
   va_end(ap);
   return FS_EXCEPTION;
 }
-  
-void discrete_meanvar(int n, SSINT *values, double *probs, 
-		      double *mean, double *var)
-{
-  int i = 0;
-  double T = 0.0;
-  *mean = 0.0;
-  *var = 0.0;
 
-  for (i=0; i < n; i++)
-    {
-      T = values[i] * probs[i];
-      *mean += T;
-      T *= values[i];
-      *var += T;
-    }
-  *var = *var - (*mean * *mean); 
-}
+  
+/********************************************************************/ 
+/*                                                                  */
+/*      Error checking wrappers to malloc, calloc and realloc       */ 
+/*                                                                  */
+/********************************************************************/ 
 
 void *callocec(long int number, long int size)
 {
@@ -69,6 +64,13 @@ void *reallocec(void *pt, long int size)
     Throw FSexcept(NO_MEM, "No Memory!");
   return(memp);
 }
+
+/********************************************************************/ 
+/*                                                                  */
+/*                    Unix path processing                          */ 
+/*                                                                  */
+/********************************************************************/ 
+
 
 int split_base_dir(const char *full_name, char **basename, 
 		   char **dirname)
@@ -135,159 +137,12 @@ int cat_base_dir(char **full_name, const char *basename,
   return 1;
 }
 
-/* ************* seedhist module *************** */
 
-
-SEED_HIST *seedhist_init(SLINT min_bin, SLINT max_bin)
-{
-  SEED_HIST *hist;
-  if (min_bin > max_bin) return NULL;
-  hist = callocec(1, sizeof(SEED_HIST));
-  hist->min_bin = min_bin;
-  hist->no_bins = 1 + max_bin - min_bin;
-  hist->freq = callocec(hist->no_bins, sizeof(ULINT));
-  return hist;
-}
-
-void seedhist_init1(SEED_HIST *hist, SLINT min_bin, SLINT max_bin)
-{
-  if (min_bin > max_bin) 
-    {
-#if DEBUG > 0
-      printf("Invalid seedhist_init1 range.\n");
-#endif
-      return;
-    }
-  hist->min_bin = min_bin;
-  hist->no_bins = 1 + max_bin - min_bin;
-  hist->freq = (ULINT *) callocec(hist->no_bins, sizeof(ULINT));
-}
-
-
-int seedhist_clear(SEED_HIST *hist)
-{
-  free(hist->freq);
-  free(hist);
-  return 1;
-}
-
-int seedhist_clear1(SEED_HIST *hist)
-{
-  free(hist->freq);
-  return 1;
-}
-
-void seedhist_add_acount(SEED_HIST *hist, SLINT bin)
-{
-  SLINT old_no_bins;
-  SLINT dsize;
-  ULINT *old_freq;
-
-  if (bin < hist->min_bin)
-    {
-#if DEBUG > 0
-      printf("Smaller bin!\n");
-#endif
-      old_no_bins = hist->no_bins;
-      dsize = hist->min_bin - bin;
-      hist->min_bin = bin;
-      hist->no_bins += dsize;
-      old_freq = hist->freq;
-      hist->freq = callocec(hist->no_bins, sizeof(ULINT));
-      memcpy(hist->freq+dsize, old_freq, old_no_bins*sizeof(ULINT)); 
-      free(old_freq);
-    }
-  else if (bin > (hist->no_bins+hist->min_bin-1))
-    {
-#if DEBUG > 0
-      printf("Larger bin!\n");
-#endif
-      dsize = bin - hist->no_bins - hist->min_bin +1;
-      old_no_bins = hist->no_bins;
-      old_freq = hist->freq;
-      hist->no_bins += dsize;
-      hist->freq = reallocec(hist->freq, hist->no_bins*sizeof(ULINT));
-      memset(hist->freq+old_no_bins, 0, dsize*sizeof(ULINT));
-    }
-  hist->freq[bin - hist->min_bin]++;
-  hist->attainable_pts++;
-  return ;
-}
-
-
-int seedhist_merge(SEED_HIST *hist1, SEED_HIST *hist2)
-{
-  ULINT i;
-  if ((hist1->no_bins != hist2->no_bins) || 
-      (hist1->min_bin != hist2->min_bin))
-    return 0;
-
-  hist1->attainable_pts += hist2->attainable_pts;
-  hist1->unattainable_pts += hist2->unattainable_pts;
-  for (i=0; i < hist1->no_bins; i++)
-    hist1->freq[i] += hist2->freq[i];
-  return 1;
-}
-
-int seedhist_read(SEED_HIST *hist, FILE *stream)
-{
-  fread(&hist->no_bins, sizeof(SLINT), 1, stream);
-  fread(&hist->min_bin, sizeof(SLINT), 1, stream);
-  fread(&hist->attainable_pts, sizeof(ULINT), 1, stream);
-  fread(&hist->unattainable_pts, sizeof(ULINT), 1, stream);
-  hist->freq = callocec(hist->no_bins, sizeof(ULINT));
-  fread(hist->freq, sizeof(ULINT), hist->no_bins, stream);
-  return 1;
-}
-
-int seedhist_write(SEED_HIST *hist, FILE *stream)
-{
-  fwrite(&hist->no_bins, sizeof(SLINT), 1, stream);
-  fwrite(&hist->min_bin, sizeof(SLINT), 1, stream);
-  fwrite(&hist->attainable_pts, sizeof(ULINT), 1, stream);
-  fwrite(&hist->unattainable_pts, sizeof(ULINT), 1, stream);
-  fwrite(hist->freq, sizeof(ULINT), hist->no_bins, stream);
-  return 1;
-}
-
-int seedhist_print(SEED_HIST *hist, FILE *stream)
-{
-  ULINT i;
-  ULINT min0 = 0;
-  ULINT max0 = hist->no_bins -1;
-  ULINT counted_seq = 0;
-
-  while(hist->freq[min0] == 0 && min0 < hist->no_bins)
-    min0++;
-  while(hist->freq[max0] == 0 && max0 > 0)
-    max0--;
-#if DEBUG > 5
-  printf("min0 = %ld\n", min0);
-  printf("max0 = %ld\n", max0);
-  fflush(stdout);
-#endif
-  for (i=min0; i <= max0; i++)
-    counted_seq += hist->freq[i];
-
-  fprintf(stream, "Points: %ld %ld\n", hist->attainable_pts,
-	  hist->unattainable_pts);
-#if DEBUG > 5
-  fprintf(stream, "Counted points: %ld\n", counted_seq);
-#endif
-  fprintf(stream, "Values: %ld\n", max0+1-min0);
-  fprintf(stream, "%12s %12s\n", "Value", "Frequency");
-
-  for (i=min0; i <= max0; i++)
-    fprintf(stream, "%12ld %12ld\n", hist->min_bin+i, hist->freq[i]);
-  fprintf(stream, "\n");
-  return 1;
-}
-
-
-
-
-/* ************* End of seedhist module *********** */
-
+/********************************************************************/ 
+/*                                                                  */
+/*                    String I/O                                    */ 
+/*                                                                  */
+/********************************************************************/ 
 
 void fwrite_string(char *s, FILE *fp) 
 {
@@ -304,6 +159,12 @@ void fread_string(char **s, FILE *fp)
   fread(*s, sizeof(char), len, fp);           
 }
 
+
+/********************************************************************/ 
+/*                                                                  */
+/*                Comparison functions for qsort                    */ 
+/*                                                                  */
+/********************************************************************/ 
 
 int compare_int(const void *M1, const void *M2)
 {
@@ -341,6 +202,12 @@ int compare_dbl(const void *M1, const void *M2)
     return 0;
 }
 
+/********************************************************************/ 
+/*                                                                  */
+/*                Check validity of strings                         */ 
+/*                                                                  */
+/********************************************************************/ 
+
 int check_word_alphabet(const char *word, int word_len,
 			const char *alphabet, int alphabet_len)
 {
@@ -355,6 +222,12 @@ int check_word_alphabet(const char *word, int word_len,
 
   return 1;
 }
+
+/********************************************************************/ 
+/*                                                                  */
+/*                Print into buffer that can grow                   */ 
+/*                                                                  */
+/********************************************************************/ 
 
 #define ABSPRINTF_BUF 256
 void absprintf(char **buf, int *size, int *len, const char *fmt, ...)
@@ -379,99 +252,3 @@ void absprintf(char **buf, int *size, int *len, const char *fmt, ...)
   *len += n;
   va_end(ap);
 } 
-
-
-
-
-
-
-/* Getline implementation */
-
-/* GNU mailutils - a suite of utilities for electronic mail
-   Copyright (C) 1999, 2000, 2001, 2002 Free Software Foundation, Inc.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Library Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
-
-/* First implementation by Alain Magloire */
-
-int
-getline (char **lineptr, size_t *n, FILE *stream)
-{
-  return getdelim (lineptr, n, '\n', stream);
-}
-
-#ifndef HAVE_GETDELIM
-
-/* Default value for line length.  */
-static const int line_size = 128;
-
-int
-getdelim (char **lineptr, size_t *n, int delim, FILE *stream)
-{
-  int indx = 0;
-  int c;
-
-  /* Sanity checks.  */
-  if (lineptr == NULL || n == NULL || stream == NULL)
-    return -1;
-
-  /* Allocate the line the first time.  */
-  if (*lineptr == NULL)
-    {
-      *lineptr = malloc (line_size);
-      if (*lineptr == NULL)
-	return -1;
-      *n = line_size;
-    }
-
-  while ((c = getc (stream)) != EOF)
-    {
-      /* Check if more memory is needed.  */
-      if (indx >= *n)
-	{
-	  *lineptr = realloc (*lineptr, *n + line_size);
-	  if (*lineptr == NULL)
-	    return -1;
-	  *n += line_size;
-	}
-
-      /* Push the result in the line.  */
-      (*lineptr)[indx++] = c;
-
-      /* Bail out.  */
-      if (c == delim)
-	break;
-    }
-
-  /* Make room for the null character.  */
-  if (indx >= *n)
-    {
-      *lineptr = realloc (*lineptr, *n + line_size);
-      if (*lineptr == NULL)
-       return -1;
-      *n += line_size;
-    }
-
-  /* Null terminate the buffer.  */
-  (*lineptr)[indx++] = 0;
-
-  /* The last line may not have the delimiter, we have to
-   * return what we got and the error will be seen on the
-   * next iteration.  */
-  return (c == EOF && (indx - 1) == 0) ? -1 : indx - 1;
-}
-
-#endif /* HAVE_GETDELIM */
-
