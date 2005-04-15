9dc5ba2a6c15f4e54bf03135963f6e1dc8737eda
Aleksandar Stojmirovic <aleksandar@stojmirovic.org>
2005-04-15 23:43:42 +0000

    Moved ufragdb into new files and new repository.

--- FSindex/src/include/fastadb.h
+++ FSindex/src/include/fastadb.h
@@ -8,7 +8,6 @@
 
 #include "misclib.h"
 
-#define UFRAGDB
 #define MAX_STORAGE_ROWS 2
 #define MAX_SEQUENCES 10000
 
@@ -183,74 +182,6 @@ char *MIXED_ITER_next(MIXED_ITER *iterator, ULINT *len);
 MY_INLINE
 void MIXED_ITER_reset(MIXED_ITER *iterator);
 
-#ifdef UFRAGDB
-/********************************************************************/    
-/********************************************************************/    
-/***                                                              ***/
-/***               ufragdb                                        ***/ 
-/***                                                              ***/
-/********************************************************************/    
-/********************************************************************/    
-
-typedef struct 
-{
-  char *db_name;         /* Fasta database name                     */
-  int db_name_len;       /* Fasta db name length                    */
-  SEQUENCE_DB *sdb;      /* Fasta database                          */
-  ULINT frag_len;        /* Fragment length                         */
-  int skip;              /* How many fragments to skip (1=none)     */
-  FILE *fptr;            /* File handle                             */
-
-  ULINT *pts;            /* Offsets of unique sequences             */
-  ULINT pts_size;        /* Full Number of 'unique' points          */
-  ULINT upts_size;       /* Number of points without duplicates     */
-  ULINT dpts_size;       /* Number of points with duplicates        */
-  ULINT *dup_offset;     /* Offsets of duplicates in heap           */
-  ULINT *dup_heap;       /* Heap containing all the duplicates      */ 
-  ULINT dup_heap_size;			    
-  ULINT cur_pt;          /* Current point                           */
-
-  ULINT pts0;            /* Buffered read - starting index          */
-  ULINT pts1;            /* Buffered read - one past end index      */
-} UFRAG_DB;
-
-
-UFRAG_DB *ufragdb_init(void);
-
-void ufragdb_del(UFRAG_DB *udb);
-
-void ufragdb_create(UFRAG_DB *udb, const char *db_name, 
-		    ULINT frag_len, int skip);
-
-void ufragdb_load(UFRAG_DB *udb, const char *udb_name,
-		  int options);
-
-void ufragdb_save(UFRAG_DB *udb, const char *udb_name);
-
-void ufragdb_read(UFRAG_DB *udb, FILE *fp, char *db_dir);
-
-void ufragdb_write(UFRAG_DB *udb, FILE *fp);
-
-
-
-ULINT ufragdb_get_nopts(UFRAG_DB *udb);
-
-int ufragdb_get_ufrag(UFRAG_DB *udb, ULINT i);
-
-void ufragdb_init_ufrags(UFRAG_DB *udb, ULINT i0);
-
-int ufragdb_get_next_ufrag(UFRAG_DB *udb);
-
-int ufragdb_get_dfrags(UFRAG_DB *udb, ULINT frag_offset, 
-		       ULINT **dfrags, ULINT *dsize);
-
-int ufragdb_get_dfrags2(UFRAG_DB *udb, ULINT i, 
-			ULINT **dfrags, ULINT *dsize);
-
-void ufragdb_print(UFRAG_DB *udb, FILE *stream);
-
-int cmp_ufrags(const void *S1, const void *S2);
-#endif /* #ifdef UFRAGDB */
 
 /********************************************************************/    
 /********************************************************************/    
