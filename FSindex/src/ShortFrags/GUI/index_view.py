f2539f26f9eae746e4032db467711efc472a0fae
Aleksandar Stojmirovic <aleksandar@stojmirovic.org>
2005-07-13 06:29:26 +0000

    New PFMF client added. Replaces FE.

--- FSindex/src/ShortFrags/GUI/index_view.py
+++ FSindex/src/ShortFrags/GUI/index_view.py
@@ -2,10 +2,11 @@ from ShortFrags.GUI.view import View
 from ShortFrags.GUI.text_display import TextDisplay
 
 class IndexView(TextDisplay, View):
-    def __init__(self, parent=None):
+    def __init__(self, parent, PFMF_client):
         TextDisplay.__init__(self, parent, label='Index')
         View.__init__(self)
-
+        self.PFMF_client = PFMF_client
+        
     def reset(self, state):
-        text = state['FE'].search_client.index_data_str()
+        text = self.PFMF_client.index_data_str()
         self.set_text('Index', text)
