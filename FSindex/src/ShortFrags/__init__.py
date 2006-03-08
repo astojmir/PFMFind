7a6e874bbd50fea683b060a825b7b2d3c29a1486
Aleksandar Stojmirovic <aleksandar@stojmirovic.org>
2006-03-08 22:10:20 +0000

    Changed the default installation directories so that they work without root permissions.

--- FSindex/src/ShortFrags/__init__.py
+++ FSindex/src/ShortFrags/__init__.py
@@ -26,5 +26,7 @@ __all__ = ['GUI', 'Expt', 'Setup', 'plugins']
 import sys
 from os.path import join
 
-CONFIG_DATA_DIR = join(sys.exec_prefix, 'share/ShortFrags/setup_config')
-SQL_DATA_DIR =  join(sys.exec_prefix, 'share/ShortFrags/sql-schema')
+#CONFIG_DATA_DIR = join(sys.exec_prefix, 'share/ShortFrags/setup_config')
+#SQL_DATA_DIR =  join(sys.exec_prefix, 'share/ShortFrags/sql-schema')
+CONFIG_DATA_DIR = join('share', 'ShortFrags', 'setup_config')
+SQL_DATA_DIR = join('share', 'ShortFrags', 'sql-schema')
